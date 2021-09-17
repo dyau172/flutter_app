0bf01da8d3420aa2932ecf5e5d0735d883f8ea89
Donna Yau <donna.yau@msn.com>
2021-09-17 20:30:51 +0100

    add FAB

--- flutter_app/lib/views/home.dart
+++ flutter_app/lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/create_note.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,15 +16,30 @@ class _HomePageState extends State<HomePage> {
           children: <Widget>[
             Text(
               'Flutter',
-              style: TextStyle(fontSize: 20),
+              style: TextStyle(fontSize: 28.0),
             ),
             Text(
               'App',
-              style: TextStyle(fontSize: 20, color: Colors.purple),
+              style: TextStyle(fontSize: 28.0, color: Colors.purple),
             ),
           ],
         ),
       ),
+      floatingActionButton: Container(
+          padding: EdgeInsets.only(bottom: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FloatingActionButton(
+                backgroundColor: Colors.purple,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CreateNote()));
+                },
+                child: Icon(Icons.add),
+              )
+            ],
+          )),
     );
   }
 }
